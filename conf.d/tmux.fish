9abd88206a51d3e8a30fe205bfe44f3385e616b6
dentych <dennis@tychsen.me>
2023-10-16 21:34:27 +0200

    Attach if exist tmux

--- conf.d/tmux.fish
+++ conf.d/tmux.fish
@@ -1,10 +1,9 @@
-if status is-interactive
-and not set -q TMUX
-	exec tmux
-end
-
 if not test -e ~/.tmux.conf
-	echo "Tmux conf not detected. Creating symlink"
-	ln -s ~/.config/fish/conf.d/tmux.conf ~/.tmux.conf
+    echo "Tmux conf not detected. Creating symlink"
+    ln -s ~/.config/fish/conf.d/tmux.conf ~/.tmux.conf
 end
 
+if status is-interactive
+    and not set -q TMUX
+    exec tmux new-session -A -s main
+end
